56b29a1308559ba4ccc7587e970e74d74055d51d
Anri <anri0806@hotmail.co.jp>
2023-07-12 13:32:01 -0400

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,19 @@
 def oxford_comma(items):
-    return None
+    
+    # if len(items) > 1:
+    #     items[-1] = "and " + items[-1]
+    
+    # if len(items) > 2:
+    #     return ', '.join(items)
+    # else:
+    #     return ' '.join(items)
+    
+    if len(items) == 1:
+        return ''.join(items)
+    
+    if len(items) == 2:
+        return ' and '.join(items)
+    
+    if len(items) >= 3:
+        items[-1] = "and " + items[-1]
+        return ', '.join(items)
